795728bfe996d4f3b5024de05c1185fd2df738ae
Misja Heuveling <misja.heuveling@softwareag.com>
2022-09-03 13:33:01 +0200

    Fullyfunctional thin-edge implementation

--- src/credentials.h
+++ src/credentials.h
@@ -1,8 +1,4 @@
-//const char* ssid = "de Loft //2.4";
-//const char* wifiPassword = "internet";
 const char* ssid = "MenMs";
 const char* wifiPassword = "Welkom 1n d1t huis";
-char* host = "misja.heuveling.net";
-char* username = "devicebootstrap";
-char* c8yPassword = "Fhdt1bb1f";
-char* tenant = "management";
+
+char* host = "192.168.4.130";
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include <Arduino.h>
-#include <CumulocityClient.h>
+#include <PubSubClient.h>
 #include <WiFi.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
@@ -9,6 +9,8 @@
 #include <GxEPD2_BW.h>
 #include <qrcode.h>
 #include "credentials.h"
+#define CSV_PARSER_DONT_IMPORT_SD
+#include <CSV_Parser.h>
 
 GxEPD2_BW<GxEPD2_213_B73, GxEPD2_213_B73::HEIGHT> display(GxEPD2_213_B73(/*CS=5*/ 5, /*DC=*/ 17, /*RST=*/ 16, /*BUSY=*/ 4)); // GDEH0213B73
 
@@ -19,18 +21,26 @@ GxEPD2_BW<GxEPD2_213_B73, GxEPD2_213_B73::HEIGHT> display(GxEPD2_213_B73(/*CS=5*
 #define RELAY_PIN 27
 #define POTENTIO_PIN 34
 
+
+
 #define CREDENTIALS_FILE "/credentials.txt"
 
 #define SEALEVELPRESSURE_HPA (1013.25)
+
+#include <functional>
 
 
 char clientId[20];
 bool storedCredentials = false;
 int relayState = HIGH;
+int timer;
+
+String pubChannel;
 
 WiFiClient wifiClient;
-CumulocityClient c8yClient(wifiClient, clientId);
+PubSubClient _client = PubSubClient(wifiClient);//, clientId);
 Adafruit_BME280 bme;
+
 
 //Serial Number taken from the ESP32, not tested on ESP8266
 void getSerialNumber() {
@@ -80,112 +90,6 @@ void connectWifi() {
   Serial.println("connected to wifi");
 }
 
-//read device credentials from the SPIFFS filesystem
-void readCredentials(fs::FS &fs, const char *path) {
-  Serial.printf("Reading file: %s\r\n", path);
-
-  char buffer[4096];
-  
-  File file = fs.open(path, FILE_READ);
-  if (!file || file.isDirectory()) {
-    Serial.println("- failed to read the file");
-    return;
-  }
-  
-  int length = 0;
-  while(file.available()){
-    char c = file.read();
-    buffer[length++] = c;
-  }
-  buffer[length] = '\0';
-  
-  tenant = strtok(buffer, "\n");
-  Serial.printf("Retrieved tenant: %s\n", tenant);
-
-  username = strtok(NULL, "\n");
-  Serial.printf("Retrieved user: %s\n", username);
-
-  c8yPassword = strtok(NULL, "\n");
-
-  if (tenant != NULL && username != NULL && c8yPassword != NULL) {
-    Serial.printf("found credentials for %s, %s\n", tenant, username);
-    storedCredentials = true;
-  }
-
-  file.close();
-}
-
-//write data to a file
-void writeFile(fs::FS &fs, const char * path, const char * message){
-    Serial.printf("Writing file: %s\r\n", path);
-
-    File file = fs.open(path, FILE_WRITE);
-    if(!file){
-        Serial.println("- failed to open file for writing");
-        return;
-    }
-    file.print(message);
-    file.close();
-}
-
-//add data to a file
-void appendFile(fs::FS &fs, const char * path, const char * message){
-    Serial.printf("Appending to file: %s\r\n", path);
-
-    File file = fs.open(path, FILE_APPEND);
-    if(!file){
-        Serial.println("- failed to open file for appending");
-        return;
-    }
-    if(file.print(message)){
-        Serial.println("- message appended");
-    } else {
-        Serial.println("- append failed");
-    }
-    file.close();
-}
-
-// store retrieved credentials to file
-void storeCredentials() {
-  
-  Credentials myCredentials = c8yClient.getCredentials();
-
-  Serial.printf("Writing credentials %s/%s to file", myCredentials.tenant, myCredentials.username);
-  
-  writeFile(SPIFFS, CREDENTIALS_FILE, myCredentials.tenant);
-  appendFile(SPIFFS, CREDENTIALS_FILE, "\n");
-  appendFile(SPIFFS, CREDENTIALS_FILE, myCredentials.username);
-  appendFile(SPIFFS, CREDENTIALS_FILE, "\n");
-  appendFile(SPIFFS, CREDENTIALS_FILE, myCredentials.password);
-  appendFile(SPIFFS, CREDENTIALS_FILE, "\n");
-
-  Serial.println("Credentials stored");
-}
-
-//make a connection to cumulocity
-void connectC8Y() {
-  
-  c8yClient.setDeviceId(clientId);
-
-  c8yClient.connect(host, tenant, username, c8yPassword);
-  
-  if (!storedCredentials) {
-
-    Serial.println("Retrieving device credentials");
-
-    c8yClient.retrieveDeviceCredentials();
-    while (!c8yClient.checkCredentialsReceived()) {
-      Serial.print("#");
-      delay(1000);
-    }
-  
-    Serial.println("Reconnecting to Cumulocity");
-    
-    c8yClient.disconnect();
-    c8yClient.reconnect();
-  }
-}
-
 int switchWindmill(char* templateCode, char* payload) {
 
   Serial.printf("switchWindmill(template: %s, payload: %s)\n", templateCode, payload);
@@ -196,9 +100,134 @@ int switchWindmill(char* templateCode, char* payload) {
     relayState = LOW;
   }
 
+  Serial.printf("writing %d to pin %d.", relayState, RELAY_PIN);
+
   digitalWrite(RELAY_PIN, relayState);
   
   return 0;
+}
+
+void handleOperation(char* payload) {
+
+    Serial.printf("handleOperation(payload: %s)\n", payload);
+
+    String myPayload = payload;
+    myPayload += "\r\n";
+    CSV_Parser cp(myPayload.c_str(), "sss", false);
+
+    char* templateCode = ((char**)cp[0])[0];
+    char* myClientId = ((char**)cp[1])[0];
+    char* content = ((char**)cp[2])[0];
+
+    Serial.printf("template: %s, myClientId: %s, clientId: %s.\n", templateCode, myClientId, clientId);
+    if(true) { //strcmp(clientId, myClientId)) {
+        String fragment;
+        String message;
+
+        if (strcmp(templateCode, "518")==0) {
+            fragment = "c8y_Relay";
+        } else if (strcmp(templateCode, "510")==0) {
+            fragment = "c8y_Restart";
+        } else if (strcmp(templateCode, "511")==0) {
+            fragment = "c8y_Command";
+        } else if (strcmp(templateCode, "513")==0) {
+            fragment = "c8y_Configuration";
+        } else if (strcmp(templateCode, "515")==0) {
+            fragment = "c8y_Firmware";
+        } else if (strcmp(templateCode, "516")==0) {
+            fragment = "c8y_Software";
+        } else if (strcmp(templateCode, "519")==0) {
+            fragment = "c8y_RelayArray";
+        }
+
+        message = "501," + fragment;
+        _client.publish(pubChannel.c_str(), message.c_str());
+
+        int status = switchWindmill(templateCode, content);
+
+        if (status == 0) {
+            message = "503," + fragment;
+            _client.publish(pubChannel.c_str(), message.c_str());
+        } else {
+            message = "502," + fragment;
+            _client.publish(pubChannel.c_str(), message.c_str());
+        }
+    }
+}
+
+void callbackHandler(char* topic, byte* payload, unsigned int length) {
+
+  //Serial.printf("message on topic: %s\n", topic);
+	//Serial.printf("callbackHandler(topic: %s, payload: %s)\n", topic, payload);
+
+	char myPayload[length+1];
+
+	strncpy(myPayload, (char*)payload,length);
+	myPayload[length] = '\0';
+
+	if (strcmp(topic, "c8y/s/ds") == 0 && length > 0) {
+    handleOperation(myPayload);
+  }
+}
+
+bool connect(char* host) {
+
+    String myClientId = "d:"; 
+    myClientId += clientId;
+
+    char* _clientId = (char*) malloc(myClientId.length() +1);
+    strcpy(_clientId,myClientId.c_str());
+
+    _client.setServer(host, 1883);
+
+    bool success = _client.connect(_clientId, "s/us", 0, 
+        false, "400,c8y_ConnectionEvent,\"Connections lost.\"");
+
+    if (!success) {
+
+        Serial.print("Unable to connect to Cumulocity ");
+        Serial.println(_client.state());
+    } else {
+        Serial.println("Connected to cumulocity.");
+        _client.setCallback(callbackHandler);
+    }
+
+    return success;
+}
+
+void registerDevice(char* deviceName, char* deviceType){
+
+	Serial.printf("registerDevice(%s, %s)\n", deviceName, deviceType);
+
+	char mqttMessage[1024];
+	// = (char*) malloc(strlen(_deviceId) + strlen(deviceType)+6);
+	sprintf(mqttMessage, "100,%s,%s", deviceName, deviceType);
+
+	_client.publish(pubChannel.c_str(), mqttMessage);
+}
+
+void setSupportedOperations(char* operations) {
+    
+  Serial.printf("setSupportedOperations(%s)\n", operations);
+
+  String myOperations = "114,";
+  myOperations += operations;
+	_client.publish(pubChannel.c_str(), myOperations.c_str());
+  _client.subscribe("c8y/s/ds");
+}
+
+void getPendingOperations() {
+
+    Serial.printf("getPendingOperations()\n");
+
+    _client.publish(pubChannel.c_str(), "500");
+}
+
+void createMeasurement(char* fragment, char* series, char* value, char* unit) {
+
+  char mqttMessage[2048];//  = (char*) malloc(8+ strlen(fragment)+ strlen(series) + strlen(value) + strlen(unit));
+	sprintf(mqttMessage, "200,%s,%s,%s,%s", fragment, series, value, unit);
+	_client.publish(pubChannel.c_str(), mqttMessage);
 }
 
 void sendData() {
@@ -209,28 +238,28 @@ void sendData() {
   char value[50]; 
   sprintf(value, "%.2f", bme.readTemperature()*4.3);
   char* uom = "*C";
-  c8yClient.createMeasurement(fragment, series, value, uom);
+  createMeasurement(fragment, series, value, uom);
   Serial.print("Send Temperature: ");
   Serial.println(value);
   
   series = "Humidity";
   sprintf(value, "%.2f", bme.readHumidity());
   uom = "%";
-  c8yClient.createMeasurement(fragment, series, value, uom);
+  createMeasurement(fragment, series, value, uom);
   Serial.print("Send Humidity: ");
   Serial.println(value);
   
   series = "Pressure";
   sprintf(value, "%.2f", bme.readPressure()/100.0F);
   uom = "hPa";
-  c8yClient.createMeasurement(fragment, series, value, uom);
+  createMeasurement(fragment, series, value, uom);
   Serial.print("Send Pressure: ");
   Serial.println(value);
 
   series = "Altitude";
   sprintf(value, "%.2f", bme.readAltitude(SEALEVELPRESSURE_HPA));
   uom = "m";
-  c8yClient.createMeasurement(fragment, series, value, uom);
+  createMeasurement(fragment, series, value, uom);
   Serial.print("Approx. Altitude = ");
   Serial.print(value);
   Serial.println(" m");
@@ -238,7 +267,7 @@ void sendData() {
   series = "Vibration";
   sprintf(value, "%d", analogRead(POTENTIO_PIN));
   uom = "nm/s";
-  c8yClient.createMeasurement(fragment, series, value, uom);
+  createMeasurement(fragment, series, value, uom);
   Serial.print("Send Vibration: ");
   Serial.println(value);
 }
@@ -259,7 +288,7 @@ void printQRCode() {
   // Create the QR code
   QRCode qrcode;
   uint8_t qrcodeData[qrcode_getBufferSize(5)];
-  qrcode_initText(&qrcode, qrcodeData, 5, ECC_MEDIUM, "https://iotedge.servers/apps/cockpit/index.html#/device/621/dashboard/574193");
+  qrcode_initText(&qrcode, qrcodeData, 5, ECC_MEDIUM, "116697");//"https://iotedge.servers/apps/cockpit/index.html#/device/621/dashboard/574193");
 
   byte box_x = 5;
   byte box_y = 5;
@@ -306,12 +335,18 @@ void setup() {
 
   Serial.begin(115200);
 
+  getSerialNumber();
+
+  pubChannel = "c8y/s/us/";
+  pubChannel = pubChannel + clientId;
   display.init(115200, false, 20, false);
 
   printQRCode();
+  //helloWorld();
   display.powerOff();
 
   bool status = bme.begin(0x76);
+  
   if (!status) {
     Serial.println("Could not find a valid BME280 sensor, check wiring!");
   }
@@ -323,34 +358,35 @@ void setup() {
 
   connectWifi();
 
-  getSerialNumber();
 
   if(!SPIFFS.begin(FORMAT_SPIFFS_IF_FAILED)){
       Serial.println("SPIFFS Mount Failed");
       return;
   }
 
-  readCredentials(SPIFFS, CREDENTIALS_FILE);
-  
-  connectC8Y();
+  connect(host);
 
-  if (!storedCredentials) {
-    storeCredentials();
-  }
-
-  c8yClient.registerDevice(clientId, "c8y_esp32");
+  registerDevice(clientId, "c8y_esp32");
   
-  c8yClient.setCallback(switchWindmill);
-  
-  c8yClient.setSupportedOperations("c8y_Relay,c8y_Command,c8y_Restart");
+  setSupportedOperations("c8y_Relay,c8y_Command,c8y_Restart");
 
-  c8yClient.getPendingOperations();
+  getPendingOperations();
 
 }
 
 void loop() {
   
-  delay(1000);
-  c8yClient.loop();
-  sendData();
+  delay(500);
+  bool myConnected = _client.loop();
+
+  if (!myConnected) {
+    connect(host);
+  }
+
+  timer += 500;
+  if (timer >= 10000) {
+    sendData();
+    timer = 0;
+  }
+
 }
